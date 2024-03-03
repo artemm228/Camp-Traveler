fb834a6ed8d4fbc78d96e86fd6cb0af82b46662a
Artem Sharaievskyi <saraevskijartem@gmail.com>
2024-03-03 16:26:01 +0100

    added files - v1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
+
   title: "Travel",
   description: "Travel UI/UX App",
 };
